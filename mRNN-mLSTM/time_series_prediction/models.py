738ba38d30c122a70e07ab4f42f976d94089e9ac
LvJia913 <66725175+LvJia913@users.noreply.github.com>
2020-07-10 10:31:12 +0800

    Add files via upload

---
+++ mRNN-mLSTM/time_series_prediction/models.py
@@ -0,0 +1,206 @@
+#Copyright (C) 2020. Huawei Technologies Co., Ltd. All rights reserved.
+
+#This program is free software; you can redistribute it and/or modify it
+# under the terms of the BSD 3-Clause License.
+
+#This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the BSD 3-Clause License for more details.
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+import layers
+
+
+class RNN(nn.Module):
+    """RNN model for time series prediction"""
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.rnn = nn.RNN(self.input_size, self.hidden_size)
+        self.hidden2output = nn.Linear(self.hidden_size, output_size)
+
+    def forward(self, input_y, hidden_state=None):
+        samples = input_y
+        rnn_out, last_rnn_hidden = self.rnn(samples, hidden_state)
+        output = self.hidden2output(rnn_out.view(-1, self.hidden_size))
+        return output.view(samples.shape[0], samples.shape[1],
+                           self.output_size), \
+               last_rnn_hidden
+
+
+class LSTM(nn.Module):
+    """LSTM model for time series prediction"""
+    def __init__(self, input_size, hidden_size, output_size):
+        super(LSTM, self).__init__()
+        self.in_size = input_size
+        self.h_size = hidden_size
+        self.out_size = output_size
+        self.lstm_cell = nn.LSTMCell(input_size, hidden_size)
+        self.output = nn.Linear(hidden_size, output_size)
+
+    def forward(self, inputs, hidden_state=None):
+        time_steps = inputs.shape[0]
+        batch_size = inputs.shape[1]
+        outputs = torch.Tensor(time_steps, batch_size, self.out_size)
+
+        for times in range(time_steps):
+            if hidden_state is None:
+                h_0 = torch.zeros(batch_size, self.h_size)
+                c_0 = torch.zeros(batch_size, self.h_size)
+                hidden_state = (h_0, c_0)
+            else:
+                h_0 = hidden_state[0]
+                c_0 = hidden_state[1]
+            h_0, c_0 = self.lstm_cell(inputs[times, :], (h_0, c_0))
+            outputs[times, :] = self.output(h_0)
+        return outputs, (h_0, c_0)
+
+
+class MRNNFixD(nn.Module):
+    """mRNN with fixed d for time series prediction"""
+    def __init__(self, input_size, hidden_size, output_size, k, bias=True):
+        super(MRNNFixD, self).__init__()
+        self.k = k
+        self.input_size = input_size
+        self.output_size = output_size
+        self.b_d = Parameter(torch.Tensor(torch.zeros(1, input_size)),
+                             requires_grad=True)
+        self.mrnn_cell = layers.MRNNFixDCell(input_size, hidden_size,
+                                             output_size, k)
+    def get_ws(self, d_values):
+        k = self.k
+        weights = [1.] * (k + 1)
+        for i in range(k):
+            weights[k - i - 1] = weights[k - i] * (i - d_values) / (i + 1)
+        return torch.cat(weights[0:k])
+
+    def get_wd(self, d_value):
+        weights = torch.ones(self.k, 1, d_value.size(1), dtype=d_value.dtype,
+                             device=d_value.device)
+        batch_size = weights.shape[1]
+        hidden_size = weights.shape[2]
+        for sample in range(batch_size):
+            for hidden in range(hidden_size):
+                weights[:, sample, hidden] = self.get_ws(d_value[0, hidden].
+                                                         view([1]))
+        return weights.squeeze(1)
+
+    def forward(self, inputs, hidden_state=None):
+        time_steps = inputs.size(0)
+        self.d_matrix = 0.5 * F.sigmoid(self.b_d)
+        weights_d = self.get_wd(self.d_matrix)
+        for times in range(time_steps):
+            outputs, hidden_state = self.mrnn_cell(inputs[times, :], weights_d,
+                                                   hidden_state)
+        return outputs, hidden_state
+
+
+class MRNN(nn.Module):
+    """mRNN with dynamic d for time series prediction"""
+    def __init__(self, input_size, hidden_size, output_size, k, bias=True):
+        super(MRNN, self).__init__()
+        self.k = k
+        self.input_size = input_size
+        self.output_size = output_size
+        self.mrnn_cell = layers.MRNNCell(input_size, hidden_size,
+                                         output_size, k)
+
+    def forward(self, inputs, hidden_state=None):
+        time_steps = inputs.size(0)
+        batch_size = inputs.size(1)
+        outputs = torch.Tensor(time_steps, batch_size, self.output_size)
+        for times in range(time_steps):
+            outputs[times, :], hidden_state = self.mrnn_cell(inputs[times, :],
+                                                             hidden_state)
+        return outputs, hidden_state
+
+
+class MLSTMFixD(nn.Module):
+    """mLSTM with fixed d for time series prediction"""
+    def __init__(self, input_size, hidden_size, k, output_size):
+        super(MLSTMFixD, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.k = k
+        self.d_values = Parameter(torch.Tensor(torch.zeros(1, hidden_size)),
+                                  requires_grad=True)
+        self.output_size = output_size
+        self.mlstm_cell = layers.MLSTMFixDCell(self.input_size,
+                                               self.hidden_size,
+                                               self.output_size,
+                                               self.k)
+        self.sigmoid = nn.Sigmoid()
+
+    def get_w(self, d_values):
+        k = self.k
+        weights = [1.] * (k + 1)
+        for i in range(k):
+            weights[k - i - 1] = weights[k - i] * (i - d_values) / (i + 1)
+        return torch.cat(weights[0:k])
+
+    def get_wd(self, d_value):
+        weights = torch.ones(self.k, 1, d_value.size(1), dtype=d_value.dtype,
+                             device=d_value.device)
+        batch_size = weights.shape[1]
+        hidden_size = weights.shape[2]
+        for sample in range(batch_size):
+            for hidden in range(hidden_size):
+                weights[:, sample, hidden] = self.get_w(d_value[0, hidden].
+                                                        view([1]))
+        return weights.squeeze(1)
+
+    def forward(self, inputs, hidden_states=None):
+        if hidden_states is None:
+            hidden = None
+            h_c = None
+        else:
+            hidden = hidden_states[0]
+            h_c = hidden_states[1]
+        time_steps = inputs.shape[0]
+        batch_size = inputs.shape[1]
+        outputs = torch.zeros(time_steps, batch_size, self.output_size,
+                              dtype=inputs.dtype, device=inputs.device)
+        self.d_values_sigmoid = 0.5 * F.sigmoid(self.d_values)
+        weights_d = self.get_wd(self.d_values_sigmoid)
+        for times in range(time_steps):
+            outputs[times, :], hidden, h_c = self.mlstm_cell(inputs[times, :],
+                                                             hidden,
+                                                             h_c,
+                                                             weights_d)
+        return outputs, (hidden, h_c)
+
+
+class MLSTM(nn.Module):
+    """mLSTM with dynamic d for time series prediction"""
+    def __init__(self, input_size, hidden_size, k, output_size):
+        super(MLSTM, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.k = k
+        self.output_size = output_size
+        self.mlstm_cell = layers.MLSTMCell(self.input_size, self.hidden_size,
+                                           self.k, self.output_size)
+
+    def forward(self, inputs, hidden_state=None):
+        if hidden_state is None:
+            hidden = None
+            h_c = None
+            d_values = None
+        else:
+            hidden = hidden_state[0]
+            h_c = hidden_state[1]
+            d_values = hidden_state[2]
+        time_steps = inputs.shape[0]
+        batch_size = inputs.shape[1]
+        outputs = torch.zeros(time_steps, batch_size, self.output_size,
+                              dtype=inputs.dtype, device=inputs.device)
+        for times in range(time_steps):
+            outputs[times, :], hidden, h_c, d_values = \
+                self.mlstm_cell(inputs[times, :], hidden, h_c, d_values)
+        return outputs, (hidden, h_c, d_values)
+
---
+++ mRNN-mLSTM/time_series_prediction/train.py
@@ -0,0 +1,220 @@
+#Copyright (C) 2020. Huawei Technologies Co., Ltd. All rights reserved.
+
+#This program is free software; you can redistribute it and/or modify it under
+#the terms of the BSD 3-Clause License.
+
+#This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the BSD 3-Clause License for more details.
+
+import argparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from copy import deepcopy
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import math
+import time
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from utils import create_dataset, to_torch
+import time_series_prediction.models as models
+
+# initialization
+PARSER = argparse.ArgumentParser()
+# In time series prediction scenario, There are four dataset:
+# 'tree7', 'traffic', 'arfima', 'DJI'.
+PARSER.add_argument('--dataset', type=str, default='traffic',
+                    help='The test dataset')
+# In review classification scenario,
+# RNN, LSTM, mRNN_fixD, mLSTM_fixD, mRNN, mLSTM are tested.
+PARSER.add_argument('--algorithm', type=str, default='mLSTM',
+                    help='The test algorithm')
+PARSER.add_argument('--epochs', type=int, default=1000,
+                    help='Number of epochs to train.')
+PARSER.add_argument('--lr', type=float, default=0.01,
+                    help='Initial learning rate.')
+PARSER.add_argument('--hidden_size', type=int, default=1,
+                    help='Number of hidden units.')
+PARSER.add_argument('--input_size', type=int, default=1,
+                    help='Number of input units, as for time series it is 1.')
+PARSER.add_argument('--output_size', type=int, default=1,
+                    help='Number of output units, as for time series it is 1.')
+PARSER.add_argument('--K', type=int, default=100,
+                    help='Truncate the infinite summation at lag K.')
+PARSER.add_argument('--patience', type=int, default=100, help='Patience.')
+FLAGS = PARSER.parse_args()
+
+
+def main():
+    batch_size = 1
+    start = 0
+    end = 100
+    # read data
+    df_data = pd.read_csv('../data/time_series_prediction/'
+                          + FLAGS.dataset + '.csv')
+    # split train/val/test
+    if FLAGS.dataset == 'tree7':
+        train_size = 2500
+        validate_size = 1000
+    if FLAGS.dataset == 'DJI':
+        train_size = 2500
+        validate_size = 1500
+    if FLAGS.dataset == 'traffic':
+        train_size = 1200
+        validate_size = 200
+    if FLAGS.dataset == 'arfima':
+        train_size = 2000
+        validate_size = 1200
+    rmse_list = []
+    mae_list = []
+    for i in range(start, end):
+        seed = i
+        print('seed ----------------------------------', seed)
+        data = np.array(df_data["x"])
+        data = data.reshape(-1, 1)
+        # normalize the data
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        data = scaler.fit_transform(data)
+        # use this function to prepare the data for modeling
+        data_x, data_y = create_dataset(data, look_back=1)
+
+        # split into train and test sets
+        train_x, train_y = data_x[0:train_size], data_y[0:train_size]
+        validate_x, validate_y = data_x[train_size:train_size +
+                                                   validate_size], \
+                                 data_y[train_size:train_size +
+                                                   validate_size]
+        test_x, test_y = data_x[train_size + validate_size:len(data_y)], \
+                         data_y[train_size + validate_size:len(data_y)]
+
+        # reshape input to be [time steps,samples,features]
+        train_x = np.reshape(train_x,
+                             (train_x.shape[0], batch_size, FLAGS.input_size))
+        validate_x = np.reshape(validate_x, (validate_x.shape[0],
+                                             batch_size, FLAGS.input_size))
+
+        test_x = np.reshape(test_x,
+                            (test_x.shape[0], batch_size, FLAGS.input_size))
+        train_y = np.reshape(train_y,
+                             (train_y.shape[0], batch_size, FLAGS.output_size))
+        validate_y = np.reshape(validate_y, (validate_y.shape[0],
+                                             batch_size, FLAGS.output_size))
+        test_y = np.reshape(test_y, (test_y.shape[0],
+                                     batch_size, FLAGS.output_size))
+
+        torch.manual_seed(seed)
+        # initialize model
+        if FLAGS.algorithm == 'RNN':
+            model = models.RNN(input_size=FLAGS.input_size,
+                               hidden_size=FLAGS.hidden_size,
+                               output_size=FLAGS.output_size)
+        elif FLAGS.algorithm == 'LSTM':
+            model = models.LSTM(input_size=FLAGS.input_size,
+                                hidden_size=FLAGS.hidden_size,
+                                output_size=FLAGS.output_size)
+        elif FLAGS.algorithm == 'mRNN_fixD':
+            model = models.MRNNFixD(input_size=FLAGS.input_size,
+                                    hidden_size=FLAGS.hidden_size,
+                                    output_size=FLAGS.output_size,
+                                    k=FLAGS.K)
+        elif FLAGS.algorithm == 'mRNN':
+            model = models.MRNN(input_size=FLAGS.input_size,
+                                hidden_size=FLAGS.hidden_size,
+                                output_size=FLAGS.output_size,
+                                k=FLAGS.K)
+        elif FLAGS.algorithm == 'mLSTM_fixD':
+            model = models.MLSTMFixD(input_size=FLAGS.input_size,
+                                     hidden_size=FLAGS.hidden_size,
+                                     output_size=FLAGS.output_size,
+                                     k=FLAGS.K)
+        elif FLAGS.algorithm == 'mLSTM':
+            model = models.MLSTM(input_size=FLAGS.input_size,
+                                 hidden_size=FLAGS.hidden_size,
+                                 output_size=FLAGS.output_size,
+                                 k=FLAGS.K)
+        else:
+            print('Algorithm selection ERROR!!!')
+        criterion = nn.MSELoss()
+        optimizer = optim.Adam(model.parameters(), lr=FLAGS.lr)
+        best_loss = np.infty
+        best_train_loss = np.infty
+        stop_criterion = 1e-5
+        rec = np.zeros((FLAGS.epochs, 3))
+        epoch = 0
+        val_loss = -1
+        train_loss = -1
+        cnt = 0
+
+        def train():
+            model.train()
+            optimizer.zero_grad()
+            target = torch.from_numpy(train_y).float()
+            output, hidden_state = model(torch.from_numpy(train_x).float())
+            with torch.no_grad():
+                val_y, _ = model(torch.from_numpy(validate_x).float(),
+                                 hidden_state)
+                target_val = torch.from_numpy(validate_y).float()
+                val_loss = criterion(val_y, target_val)
+
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            return loss, val_loss
+
+        def compute_test(best_model):
+            model = best_model
+            train_predict, hidden_state = model(to_torch(train_x))
+            train_predict = train_predict.detach().numpy()
+            val_predict, hidden_state = model(to_torch(validate_x),
+                                              hidden_state)
+            test_predict, _ = model(to_torch(test_x), hidden_state)
+            test_predict = test_predict.detach().numpy()
+            # invert predictions
+            test_predict_r = scaler.inverse_transform(test_predict[:, 0, :])
+            test_y_r = scaler.inverse_transform(test_y[:, 0, :])
+            # calculate error
+            test_rmse = math.sqrt(mean_squared_error(test_y_r[:, 0],
+                                                     test_predict_r[:, 0]))
+            test_mape = (abs((test_predict_r[:, 0] - test_y_r[:, 0]) /
+                             test_y_r[:, 0])).mean()
+            test_mae = mean_absolute_error(test_predict_r[:, 0],
+                                           test_y_r[:, 0])
+            return test_rmse, test_mape, test_mae
+
+        while epoch < FLAGS.epochs:
+            _time = time.time()
+            loss, val_loss = train()
+            if val_loss < best_loss:
+                best_loss = val_loss
+                best_epoch = epoch
+                best_model = deepcopy(model)
+            # stop_criteria = abs(criterion(val_Y, target_val) - val_loss)
+            if (best_train_loss - loss) > stop_criterion:
+                best_train_loss = loss
+                cnt = 0
+            else:
+                cnt += 1
+            if cnt == FLAGS.patience:
+                break
+            # save training records
+            time_elapsed = time.time()-_time
+            rec[epoch, :] = np.array([loss, val_loss, time_elapsed])
+            print("epoch: {:2.0f} train_loss: {:2.5f} val_loss: {:2.5f} "
+                  "time: {:2.1f}s".format(epoch, loss.item(), val_loss.item(),
+                                          time_elapsed))
+            epoch = epoch + 1
+
+        # make predictions
+        test_rmse, test_mape, test_mae = compute_test(best_model)
+
+        rmse_list.append(test_rmse)
+        mae_list.append(test_mae)
+        print('RMSE:{}'.format(rmse_list))
+        print('MAE:{}'.format(mae_list))
+
+
+
+if __name__ == "__main__":
+    main()
