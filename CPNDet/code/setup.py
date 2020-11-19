133e624974199bcc30f8e44622159b87b850c854
Kaiwen Duan <kaiwenduan@outlook.com>
2020-11-20 00:05:13 +0800

    Update setup.py

---
+++ CPNDet/code/setup.py
@@ -0,0 +1,24 @@
+strs = ('pip install mmcv==0.6.2')
+os.system(strs)
+
+strs = ('pip install terminaltables')
+os.system(strs)
+
+strs = ('cd models/py_utils/_cpools && python setup.py install --user')
+os.system(strs)
+
+strs = ('cd models/py_utils/roi_extractors/ops/roi_align && python setup.py install --user')
+os.system(strs)
+
+strs = ('cd data/coco/PythonAPI && make')
+os.system(strs)
+
+strs = ('cd external && make')
+os.system(strs)
+
+strs = ('cd models/py_utils/bbox/nms && python setup.py install --user')
+os.system(strs)
+
+strs = ('pip install numpy==1.16.0')
+os.system(strs)
+
