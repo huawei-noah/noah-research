8b00b5aef8db0dd9c87135e839ac18544d981125
Antoni Baum <antoni.baum@protonmail.com>
2021-02-02 00:03:22 +0100

    Update setup.py

--- HEBO/setup.py
+++ HEBO/setup.py
@@ -12,25 +12,14 @@ import setuptools
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
+    
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
 
 setuptools.setup(
         name        = 'HEBO',
         packages    = setuptools.find_packages(),
         description = 'Heteroscedastic evolutionary bayesian optimisation',
         long_description = long_description,
-        install_requires=[
-            'numpy>=1.15',
-            'pytest>=6.0.2',
-            'pandas>=1.0.1',
-            'torch>=1.4.0',
-            'pymoo>=0.4.1',
-            'scikit-learn>=0.22',
-            'gpytorch>=1.1.1',
-            'GPy>=1.9.9',
-            'Sphinx>=3.2.1',
-            'nbsphinx',
-            'recommonmark',
-            'sphinx_rtd_theme',
-            'notebook'
-            ],
+        install_requires = required,
         )
